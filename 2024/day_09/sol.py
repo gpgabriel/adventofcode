5f94de2921c0bc87da2e2a5a176650b6a5f38d5a
Gabriel Gabor <gabrielgabor@live.com>
2024-12-28 14:19:37 +0000

    Day 09 optimised

--- 2024/day_09/sol.py
+++ 2024/day_09/sol.py
@@ -1,20 +1,15 @@
-def find_next_i(data):
-    i = int(len(data) / 2) * 2
-    while i > 0 and data[i] == 0:
-        i -= 2
-    return int(i)
-
 def sol(data):
     res = 0
     idx = 0
+    k = int(len(data) / 2) * 2
 
     for i in range(len(data)):
         while data[i]:
             if i % 2 == 0:
                 res += (idx * (i/2))
             if i % 2 == 1:
-                # nit: find_next_i could be optimised
-                k = find_next_i(data)
+                while data[k] == 0 and k > 0:
+                    k -= 2
                 if k <= i:
                     break
                 res += (idx * (k/2))
